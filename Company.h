f83f453ddf657de759c04d45b76011bbe8c8e3db
robertmNe <robert.mihai.negoescu@gmail.com>
2026-02-22 18:45:06 -0500

    Add Company class with car inventory management

---
+++ Company.h
@@ -0,0 +1,37 @@
+#ifndef COMPANY_H
+#define COMPANY_H
+#include "Car.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Company 
+{
+
+    private:
+    Car* inventory[1000];
+    int count;
+
+    public:
+    //Constructors
+    Company();
+
+    //Copy constructor
+    Company (const Company &other);
+
+    //Destructor
+    ~Company();
+
+    //Getters
+    int getCount()const;
+
+    //Functions
+    void addCar(Car* c);
+    void removeCar(int id);
+    void findCar(int id);
+    void displayCar()const;
+
+};
+
+#endif
